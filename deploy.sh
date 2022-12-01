1cbca9ecba5196461314b4c3ffc190bb0504f32b
nqd881 <nqd881@gmail.com>
2022-12-02 01:08:01 +0700

    update travis deploy

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+docker build -t loserforever/learn-k8s-server:latest - loserforever/learn-k8s-server:$SHA -f Dockerfile ./
+
+docker push loserforever/learn-k8s-server:latest
+docker push loserforever/learn-k8s-server:$SHA
+
+kubectl apply -f ./k8s
+kubectl set image deployment/server-deployment server=loserforever/learn-k8s-server:$SHA
